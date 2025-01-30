a41c7056e7daabcfe7b198e18ffa12963bb10243
emirks <emirks88@gmail.com>
2025-01-30 10:37:08 +0000

    Bump version to 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="va-shared",
-    version="0.1.7",
+    version="0.1.8",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
