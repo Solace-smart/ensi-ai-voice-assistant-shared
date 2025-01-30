43e3a55d8e37892669974fa2bbe8ba0b40d38ca6
emirks <emirks88@gmail.com>
2025-01-30 10:38:51 +0000

    Bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="va-shared",
-    version="0.1.8",
+    version="0.1.9",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
