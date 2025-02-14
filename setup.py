cee8e5aa9c39a776f26d3b01c07ffb3c549195d8
Erkam Kavak <70749779+erkamkavak@users.noreply.github.com>
2025-02-14 21:03:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
-        "pydantic>=2.0.0",
+        "pydantic>=2.8.2",
         "firebase-admin>=6.0.0",
         "aiohttp>=3.8.0",
     ],
