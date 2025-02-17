dfad37cca384ca924471871cbf624439c4393ff6
erkam.kavak <erkam.kavak@boun.edu.tr>
2025-02-17 07:33:20 +0000

    Bump version to 0.1.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="va-shared",
-    version="0.1.15",
+    version="0.1.16",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
--- src/va_shared/version.py
+++ src/va_shared/version.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import re
 from typing import Tuple
 
-VERSION = "0.1.15"
+VERSION = "0.1.16"
 
 def get_version() -> str:
     """Get the current version."""
